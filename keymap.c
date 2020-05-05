02f51d844508efbbb617b4de2aaa2cd21af9eb4e
Magnus <magnusfredlundh@gmail.com>
2020-05-05 23:20:08 +0200

    Fixes

--- keymap.c
+++ keymap.c
@@ -15,7 +15,6 @@
  */
 
 #include QMK_KEYBOARD_H
-#include "debug.h"
 #include "action_layer.h"
 #include "version.h"
 #include "keymap_swedish.h"
@@ -74,8 +73,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // right hand
     HYPR(KC_A),       KC_6,             KC_7,             KC_8,             KC_9,             KC_0,             HYPR(KC_T),
     KC_NO,            KC_Y,             KC_U,             KC_I,             KC_O,             KC_P,             KC_BSPC,
-                      KC_H,             KC_J,             KC_K,             KC_L,             NO_ASTR,          KC_RCTRL,
-    TG(ARRO),         KC_N,             KC_M,             KC_COMM,          KC_DOT,           NO_MINS,          KC_RSFT,
+                      KC_H,             KC_J,             KC_K,             KC_L,             SE_ASTR,          KC_RCTRL,
+    TG(ARRO),         KC_N,             KC_M,             KC_COMM,          KC_DOT,           SE_MINS,          KC_RSFT,
                                         KC_RGUI,          KC_RALT,          KC_NO,            KC_NO,            MO(ARRO),
     RSFT(KC_RALT), KC_RALT,
     KC_PGUP,
@@ -106,20 +105,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
     // left hand
-    KC_TRNS,          NO_CIRC,          KC_TRNS,          KC_TRNS,          LALT(KC_4),       KC_PERC,          KC_TRNS,
+    KC_TRNS,          SE_CIRC,          KC_TRNS,          KC_TRNS,          LALT(KC_4),       KC_PERC,          KC_TRNS,
     KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,      KC_TRNS,      KC_GRAVE,          KC_TRNS,
-    KC_TRNS,          NO_AMPR,          KC_TRNS,          KC_TRNS,          NO_PIPE,          LSFT(KC_GRAVE),
-    KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          NO_MINS,          KC_SPC,
+    KC_TRNS,          SE_AMPR,          KC_TRNS,          KC_TRNS,          SE_PIPE,          LSFT(KC_GRAVE),
+    KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          SE_MINS,          KC_SPC,
     KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,
                                                                                                        KC_TRNS, KC_TRNS,
                                                                                                                 KC_TRNS,
                                                                                               KC_TRNS, KC_TRNS, KC_TRNS,
     // right hand
     KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS,        KC_TRNS,
-    KC_TRNS,          NO_QUES,          NO_LESS_MAC,      NO_GRTR_MAC,      NO_CIRC,          NO_AA,          KC_TRNS,
-                      NO_PLUS,          NO_LCBR_MAC,      NO_RCBR_MAC,      NO_OSLH,          NO_AE,          KC_TRNS,
-    KC_ENT,           NO_UNDS,          NO_LBRC,          NO_RBRC,          NO_TILD,          KC_TRNS,        KC_TRNS,
-                                        KC_GRAVE,         NO_APOS,          KC_INT1,          KC_INT2,        KC_INT3,
+    KC_TRNS,          SE_QUES,          SE_LESS_MAC,      SE_GRTR_MAC,      SE_CIRC,          SE_AA,          KC_TRNS,
+                      SE_PLUS,          SE_LCBR_MAC,      SE_RCBR_MAC,      SE_OSLH,          SE_AE,          KC_TRNS,
+    KC_ENT,           SE_UNDS,          SE_LBRC,          SE_RBRC,          SE_TILD,          KC_TRNS,        KC_TRNS,
+                                        KC_GRAVE,         SE_APOS,          KC_INT1,          KC_INT2,        KC_INT3,
     KC_TRNS, KC_TRNS,
     KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS
@@ -198,70 +197,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [NUMB] = LAYOUT_ergodox(  // layer 0 : default
     // left hand
-    NO_HALF,          KC_1,           KC_2,          KC_3,    KC_4,    KC_5,   KC_LEFT,
+    SE_HALF,          KC_1,           KC_2,          KC_3,    KC_4,    KC_5,   KC_LEFT,
     KC_DELT,          KC_Q,           KC_W,          KC_E,    KC_R,    KC_T,   TG(SYMB),
     KC_BSPC,          KC_A,           KC_S,          KC_D,    KC_F,    KC_G,
     KC_LSFT,          CTL_T(KC_Z),    KC_X,          KC_C,    KC_V,    KC_B,   ALL_T(KC_NO),
-    LT(SYMB,NO_APOS), NO_ACUT,        LALT(KC_LSFT), KC_LEFT, KC_RGHT,
+    LT(SYMB,SE_APOS), SE_ACUT,        LALT(KC_LSFT), KC_LEFT, KC_RGHT,
                                           ALT_T(KC_APP),  KC_LGUI,
                                                           KC_HOME,
                                            KC_SPC,KC_BSPC,KC_END,
     // right hand
-         KC_RGHT,      KC_6,   KC_7,   KC_8,    KC_9,    KC_0,              NO_PLUS,
-         TG(SYMB),     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,              NO_AA,
-                       KC_H,   KC_J,   KC_K,    KC_L,    NO_OSLH,            GUI_T(NO_AE),
-         MEH_T(KC_NO), KC_N,   KC_M,   KC_COMM, KC_DOT,  CTL_T(NO_MINS),    KC_RSFT,
-                               KC_UP,  KC_DOWN, NO_CIRC, NO_ASTR,           KC_FN1,
-         NO_ALGR,        CTL_T(KC_ESC),
+         KC_RGHT,      KC_6,   KC_7,   KC_8,    KC_9,    KC_0,              SE_PLUS,
+         TG(SYMB),     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,              SE_AA,
+                       KC_H,   KC_J,   KC_K,    KC_L,    SE_OSLH,            GUI_T(SE_AE),
+         MEH_T(KC_NO), KC_N,   KC_M,   KC_COMM, KC_DOT,  CTL_T(SE_MINS),    KC_RSFT,
+                               KC_UP,  KC_DOWN, SE_CIRC, SE_ASTR,           KC_FN1,
+         KC_TRNS,        CTL_T(KC_ESC),
          KC_PGUP,
          KC_PGDN,KC_TAB, KC_ENT
 ),
 
-};
-
-// Runs just one time when the keyboard initializes.
-void matrix_init_user(void) {
-
-};
-
-
-// Runs constantly in the background, in a loop.
-void matrix_scan_user(void) {
-  uint8_t layer = biton32(layer_state);
-
-  ergodox_board_led_off();
-  ergodox_right_led_1_off();
-  ergodox_right_led_2_off();
-  ergodox_right_led_3_off();
-  switch (layer) {
-    case 1:
-      ergodox_right_led_1_on();
-      break;
-    case 2:
-      ergodox_right_led_2_on();
-      break;
-    case 3:
-      ergodox_right_led_3_on();
-      break;
-    case 4:
-      ergodox_right_led_2_on();
-      ergodox_right_led_3_on();
-      break;
-    case 5:
-      ergodox_right_led_1_on();
-      ergodox_right_led_2_on();
-      break;
-    case 6:
-      ergodox_right_led_1_on();
-      ergodox_right_led_3_on();
-      break;
-    case 7:
-      ergodox_right_led_1_on();
-      ergodox_right_led_2_on();
-      ergodox_right_led_3_on();
-      break;
-    default:
-      // none
-      break;
-  }
 };
